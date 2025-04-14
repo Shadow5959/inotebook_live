6b7ae900b38ffffe885d5606f79aaa796c0ad5d3
Priyam Agarwal <priyam.agarwal2002@gmail.com>
2025-04-14 12:35:12 +0530

    updates

--- inotebook_backend/db.js
+++ inotebook_backend/db.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 
 const connectToMongo = () => {
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.error("Could not connect to MongoDB", err));
 
