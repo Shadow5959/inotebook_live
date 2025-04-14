f0ffa3bb251fe6ca94297c3666fce275983a7ba1
Priyam Agarwal <priyam.agarwal2002@gmail.com>
2025-04-14 12:17:06 +0530

    update

--- inotebook_backend/index.js
+++ inotebook_backend/index.js
@@ -1,21 +1,17 @@
 const path = require('path');
-const express = require('express');
 require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 const connectToMongo = require('./db');
 const app = require('./app');
 connectToMongo();
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../inotebook_frontend/build')));
+
 
 app.use('/api/auth', require('./routes/auth.js'));
 app.use('/api/notes', require('./routes/notes.js')); 
 
-// Catch-all handler to serve the React app for any unknown routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../inotebook_frontend/build', 'index.html'));
-});
+
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
